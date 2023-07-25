988d1d5780f5793ac7b939ab2a8cb4c1cae7b0a9
tommaay <tommy@tomster.me>
2023-07-26 03:27:07 +0700

    Setup browser router

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,4 +1,4 @@
-function App() {
+function Home() {
   return (
     <main>
       <h1>Top 10 trusted exhanges</h1>
@@ -6,4 +6,4 @@ function App() {
   );
 }
 
-export default App;
+export default Home;
