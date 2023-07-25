74e31f940898cf9e49b8a8fe8ef144bf31e260ed
tommaay <tommy@tomster.me>
2023-07-26 03:59:41 +0700

    Added a pages folder and update baseUrl in ts config

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./Home.tsx";
+import App from "./App.tsx";
 import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <App />,
   },
 ]);
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,9 @@
-function Home() {
+const Home: React.FC = () => {
   return (
     <main>
       <h1>Top 10 trusted exhanges</h1>
     </main>
   );
-}
+};
 
 export default Home;
