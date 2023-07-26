03f6cc5f95f1fb8d8d8001bf4e0c67b787f3319a
tommaay <tommy@tomster.me>
2023-07-26 17:24:07 +0700

    Add unit tests for ExchangesTable.tsx

---
+++ src/components/ExchangesTable/ExchangesTable.test.tsx
@@ -0,0 +1,89 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import ExchangesTable from "./ExchangesTable";
+import { MOCK_EXCHANGES_LIST } from "mocks/data";
+
+const exchange1 = MOCK_EXCHANGES_LIST[0];
+const exchange2 = MOCK_EXCHANGES_LIST[1];
+const exchange3 = MOCK_EXCHANGES_LIST[2];
+
+describe("ExchangesTable", () => {
+  const renderExchangesTable = async () =>
+    await render(
+      <BrowserRouter>
+        <ExchangesTable />
+      </BrowserRouter>,
+    );
+
+  it("should render the exchanges table headers", async () => {
+    renderExchangesTable();
+    await waitFor(() => {
+      expect(screen.getByText("#")).toBeVisible();
+      expect(screen.getByText("Name")).toBeVisible();
+      expect(screen.getByText("Trust Score")).toBeVisible();
+      expect(screen.getByText("24h Volume (Normalized)")).toBeVisible();
+      expect(screen.getByText("Country")).toBeVisible();
+      expect(screen.getByText("URL")).toBeVisible();
+    });
+  });
+
+  it("should render the exchanges table with Exchange 1 data", async () => {
+    renderExchangesTable();
+    await waitFor(() => {
+      const nameLink = screen.getByText(exchange1.name);
+      expect(nameLink).toBeVisible();
+      const trustScore = screen.getByText(exchange1.trust_score);
+      expect(trustScore).toBeVisible();
+      const tradeVolume = screen.getByText(
+        `${Math.round(
+          exchange1.trade_volume_24h_btc_normalized,
+        ).toLocaleString()} BTC`,
+      );
+      expect(tradeVolume).toBeVisible();
+      const country = screen.getByText(exchange1.country);
+      expect(country).toBeVisible();
+      const url = screen.getByText(exchange1.url);
+      expect(url).toBeVisible();
+    });
+  });
+
+  it("should render the exchanges table with Exchange 2 data", async () => {
+    renderExchangesTable();
+    await waitFor(() => {
+      const nameLink = screen.getByText(exchange2.name);
+      expect(nameLink).toBeVisible();
+      const trustScore = screen.getByText(exchange2.trust_score);
+      expect(trustScore).toBeVisible();
+      const tradeVolume = screen.getByText(
+        `${Math.round(
+          exchange2.trade_volume_24h_btc_normalized,
+        ).toLocaleString()} BTC`,
+      );
+      expect(tradeVolume).toBeVisible();
+      const country = screen.getByText(exchange2.country);
+      expect(country).toBeVisible();
+      const url = screen.getByText(exchange2.url);
+      expect(url).toBeVisible();
+    });
+  });
+
+  it("should render the exchanges table with Exchange 3 data", async () => {
+    renderExchangesTable();
+    await waitFor(() => {
+      const nameLink = screen.getByText(exchange3.name);
+      expect(nameLink).toBeVisible();
+      const trustScore = screen.getByText(exchange3.trust_score);
+      expect(trustScore).toBeVisible();
+      const tradeVolume = screen.getByText(
+        `${Math.round(
+          exchange3.trade_volume_24h_btc_normalized,
+        ).toLocaleString()} BTC`,
+      );
+      expect(tradeVolume).toBeVisible();
+      const country = screen.getByText(exchange3.country);
+      expect(country).toBeVisible();
+      const url = screen.getByText(exchange3.url);
+      expect(url).toBeVisible();
+    });
+  });
+});
