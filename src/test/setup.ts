7ae1b11204d748594fab5e5a9ca59aae22e5a3fc
tommaay <tommy@tomster.me>
2023-07-26 17:21:57 +0700

    Update test configurations and added a mock data file

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,7 +1,7 @@
 // Polyfill "window.fetch" used in the React component.
 import "whatwg-fetch";
 
-// Extend Jest "expect" functionality with Testing Library assertions.
+// Extend "expect" functionality with Testing Library assertions.
 import "@testing-library/jest-dom";
 
 import { server } from "mocks/server";
