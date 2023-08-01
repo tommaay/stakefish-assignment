545f72d26085b6d8baf9cf1295ef89506c283518
tommaay <tommy@tomster.me>
2023-08-01 15:48:43 +0700

    Update table styling

--- src/components/ExchangesTable/ExchangesTable.tsx
+++ src/components/ExchangesTable/ExchangesTable.tsx
@@ -23,55 +23,53 @@ const ExchangesTable: FC = () => {
 
   return (
     <div className="table-container">
-      <div className="table">
-        <table>
-          <thead>
-            <tr>
-              <th className="th">#</th>
-              <th className="th">Name</th>
-              <th className="th">Trust Score</th>
-              <th className="th">24h Volume</th>
-              <th className="th">Country</th>
-              <th className="th">URL</th>
+      <table className="table">
+        <thead>
+          <tr className="tr">
+            <th className="th">#</th>
+            <th className="th">Name</th>
+            <th className="th">Trust Score</th>
+            <th className="th">24h Volume</th>
+            <th className="th">Country</th>
+            <th className="th">URL</th>
+          </tr>
+        </thead>
+
+        <tbody className="tbody">
+          {exchanges.map((exchange) => (
+            <tr key={exchange.id} className="tr">
+              <td className="td">{exchange.trust_score_rank}</td>
+              <td className="td">
+                <div className="flex-items-center">
+                  <img
+                    src={exchange.image}
+                    alt={exchange.name}
+                    width={20}
+                    height={20}
+                  />
+
+                  <Link to={`/${exchange.id}`} className="name-link">
+                    {exchange.name}
+                  </Link>
+                </div>
+              </td>
+              <td className="td text-center">{exchange.trust_score}</td>
+              <td className="td  text-center">
+                {roundNumberAndCovertToLocale(
+                  exchange.trade_volume_24h_btc_normalized,
+                )}{" "}
+                BTC
+              </td>
+              <td className="td">{exchange.country}</td>
+              <td className="td">
+                <a href={exchange.url} target="_blank">
+                  {exchange.url}
+                </a>
+              </td>
             </tr>
-          </thead>
-
-          <tbody className="tbody">
-            {exchanges.map((exchange) => (
-              <tr key={exchange.id}>
-                <td className="td">{exchange.trust_score_rank}</td>
-                <td className="td">
-                  <div className="flex-items-center">
-                    <img
-                      src={exchange.image}
-                      alt={exchange.name}
-                      width={20}
-                      height={20}
-                    />
-
-                    <Link to={`/${exchange.id}`} className="name-link">
-                      {exchange.name}
-                    </Link>
-                  </div>
-                </td>
-                <td className="td text-center">{exchange.trust_score}</td>
-                <td className="td  text-center">
-                  {roundNumberAndCovertToLocale(
-                    exchange.trade_volume_24h_btc_normalized,
-                  )}{" "}
-                  BTC
-                </td>
-                <td className="td">{exchange.country}</td>
-                <td className="td">
-                  <a href={exchange.url} target="_blank">
-                    {exchange.url}
-                  </a>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
